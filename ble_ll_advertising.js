e866e09f437628af879f435138de2d8c8cdb1e49
doggkruse <6468053+doggkruse@users.noreply.github.com>
2021-01-17 22:01:53 -0800

    Add parsing of manufacturer data and tx power. Fix issue where crc was being parsed as an adv_tag

--- ble_ll_advertising.js
+++ ble_ll_advertising.js
@@ -1,3 +1,5 @@
+const bluetooth = require('bluetooth-numbers-database');
+
 const ADV_IND           = 0x00;
 const ADV_DIRECT_IND    = 0x01;
 const ADV_NONCONN_IND   = 0x02;
@@ -23,19 +25,19 @@ BleAdv.prototype.decode = function(pdu_type, raw_packet, offset) {
 
     // Dispatch to sub parser based on pdu_type
     switch(pdu_type) {
-        case ADV_IND:   
-        case ADV_NONCONN_IND:           
+        case ADV_IND:
+        case ADV_NONCONN_IND:
         case SCAN_RSP:
             this.adv_address = raw_packet.slice(offset, offset + 6).toAddressString();
             offset += 6;
-        
-            while(offset < raw_packet.length) {
-                let len = raw_packet[offset++] - 1;
+
+            while(offset < raw_packet.length - 3) {
+                let len = raw_packet[offset++] -1;
                 let adv_tag = raw_packet[offset++];
-        
+
                 let tag_data = raw_packet.slice(offset, offset + len);
                 offset += len;
-        
+
                 switch(adv_tag) {
                     case 0x01:
                         this.flags = {
@@ -46,6 +48,24 @@ BleAdv.prototype.decode = function(pdu_type, raw_packet, offset) {
                     case 0x08:    
                     case 0x09:
                         this.complete_local_name = tag_data.toString('utf-8');
+                        break;
+
+                    case 0x0a:
+                        this.tx_power = tag_data[0];
+                        break;
+
+                    case 0xFF:
+                        let company_id = tag_data.readUInt16LE(0);
+                        let company_name;
+
+                        if(company_id < bluetooth.companies.length) {
+                            company_name = bluetooth.companies[company_id].name;
+                        }
+                        this.manufacturer_data = {
+                            company_id: company_id,
+                            company_name: company_name,
+                            data: tag_data.slice(2)
+                        }
                         break;
 
                     default:
@@ -68,10 +88,10 @@ BleAdv.prototype.decode = function(pdu_type, raw_packet, offset) {
             this.access_address = raw_packet.readUInt32LE(offset);
             offset += 4;
 
-            // todo get the rest of the connection params   
+            // todo get the rest of the connection params
             break;
 
-        case ADV_DIRECT_IND: 
+        case ADV_DIRECT_IND:
             this.adv_address = raw_packet.slice(offset, offset + 6).toAddressString();
             offset += 6;
             this.target_address = raw_packet.slice(offset, offset + 6).toAddressString();
@@ -79,7 +99,7 @@ BleAdv.prototype.decode = function(pdu_type, raw_packet, offset) {
             // all done!
         break;
 
-        case SCAN_REQ: 
+        case SCAN_REQ:
             this.scanner_address = raw_packet.slice(offset, offset + 6).toAddressString();
             offset += 6;
             this.adv_address = raw_packet.slice(offset, offset + 6).toAddressString();
@@ -92,7 +112,7 @@ BleAdv.prototype.decode = function(pdu_type, raw_packet, offset) {
         break;
     }
 
-    if(this.emitter) { 
+    if(this.emitter) {
         this.emitter.emit(this.decoderName, this);
     }
 
